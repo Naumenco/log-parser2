6b548e404b86716e99e6fc6798f0bdd5f6032b62
Naumenco <naumenco@gmail.com>
2022-11-23 16:09:54 +0300

    Class Method 10

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,26 @@
 import java.util.Scanner; // импорт сканера
-/* Равенство. Дана следующая сигнатура метода:
+/* Многократный вызов. Дан следующий метод:
 
-public static boolean isEqual (int a, int b, int с);
-Необходимо реализовать метод таким образом, чтобы он возвращал true если все три полученных методом числа
-равны
+public static int lastNumSum(int a, int b){
+	return (a%10)+(b%10);
+}
+Выполните с его помощью сложение пяти чисел: 5, 11, 123, 14, 1, и результат выведите на экран.
+Постарайтесь выполнить задачу, используя минимально возможное количество вспомогательных переменных.
 
-Пример 1:
-a=3 b=3 с=3
-результат: true
-Пример 2:
-a=2 b=15 с=2
-результат: false
+Ответом на данное задание является код метода main, в котором происходит вызов данной функции.
  */
 public class Main {
     public static void main(String[] args) {
         // System.out.println("Введите целое число (знаков в числе 1): ");
         // Scanner reader = new Scanner(System.in);
         // char charNumber = reader.next().charAt(0);
+        int summ1=lastNumSum(5, 11);
+        int summ2=lastNumSum(123, 14);
+        int summ3=lastNumSum(summ1, summ2);
 
-        System.out.println("результат:" + isEqual(0, 0, 0));
+        System.out.println("результат:" + lastNumSum(summ3 , 1));
     }
-    public static boolean isEqual (int a, int b, int с)
-    {
-       // System.out.println(a%b);
-       // System.out.println(b%a);
-        if ((a==b)&& (b==с)) {return true;};
-        return false;
+    public static int lastNumSum(int a, int b){
+        return (a%10)+(b%10);
     }
 }
