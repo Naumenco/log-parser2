ca615f253f29e547a92d7791282b8694a6a257f1
Naumenco <naumenco@gmail.com>
2022-11-23 15:06:11 +0300

    Class Method 6

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,30 @@
 import java.util.Scanner; // импорт сканера
-/* Двузначное. Дана следующая сигнатура метода:
+/* Большая буква. Дана следующая сигнатура метода:
 
-public static boolean is2Digits(int x);
-Необходимо реализовать метод таким образом, чтобы он принимал число x и возвращал true если оно двузначное.
+public static boolean isUpperCase(char x);
+Необходимо реализовать метод таким образом, чтобы он принимал символ x и возвращал true если это большая буква в диапазоне от ‘A’ до ‘Z’.
 
 Пример 1:
-x=32
+x=’D’
 результат: true
 Пример 2:
-x=516
+x=’q’
 результат: false
  */
 public class Main {
     public static void main(String[] args) {
-       // System.out.println("Введите целое число (знаков в числе 1): ");
-       // Scanner reader = new Scanner(System.in);
-       // char charNumber = reader.next().charAt(0);
+        // System.out.println("Введите целое число (знаков в числе 1): ");
+        // Scanner reader = new Scanner(System.in);
+        // char charNumber = reader.next().charAt(0);
 
-        int myNum= 516;
-        System.out.println("результат: "+is2Digits(myNum));
-        }
-
-    public static boolean is2Digits(int x)
+        char myChar= 'D';
+        System.out.println(isUpperCase(myChar));
+    }
+    public static boolean isUpperCase(char x)
     {
-double xx= (double) x;
-        //System.out.println(xx/100);
-        if ((xx/100<1)&&(xx/100>0.09)){return true;};
+        int myNum = (int) x;
+        System.out.println(myNum);
+        if ((myNum>=65&&myNum<=90)){return true;};
         return false;
     }
 }
