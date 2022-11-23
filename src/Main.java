037414bed621a9c59c1fc5c08d6c85beba0ab966
Naumenco <naumenco@gmail.com>
2022-11-23 15:31:06 +0300

    Class Method 7

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,31 @@
 import java.util.Scanner; // импорт сканера
-/* Левый треугольник. Дана следующая сигнатура метода:
+/* Диапазон. Дана следующая сигнатура метода:
 
-public static void leftTriangle(int x);
-Необходимо реализовать метод таким образом, чтобы он выводил на экран треугольник из символов ‘*’
-у которого х символов в высоту, а количество символов в ряду совпадает с номером строки.
+public static boolean isInRange(int a, int b, int num);
+Метод принимает левую и правую границу (a и b) некоторого числового диапазона.
+Необходимо реализовать метод таким образом, чтобы он возвращал true, если num входит в указанный диапазон (включая границы).
+Обратите внимание, что отношение a и b заранее неизвестно (неясно кто из них больше, а кто меньше)
 
 Пример 1:
-x=2
-результат:
-*
-**
+a=5 b=1 num=3
+результат: true
 Пример 2:
-x=4
-результат:
-*
-**
-***
-****
+a=2 b=15 num=33
+результат: false
  */
 public class Main {
-    public static void main(String[]  args) {
-        leftTriangle(7);
+    public static void main(String[] args) {
+        // System.out.println("Введите целое число (знаков в числе 1): ");
+        // Scanner reader = new Scanner(System.in);
+        // char charNumber = reader.next().charAt(0);
+
+        System.out.println("результат:" + isInRange(2, 15, 14));
     }
-    public static void leftTriangle(int x)
+    public static boolean isInRange(int a, int b, int num)
     {
-        int count=0;
-            for (int i=0; i<x; i++){
-                for (int j=0; j<x; j++){
-                    System.out.print(" * ");
-                }
-                System.out.println("");
-            }
-        }
-
+        //System.out.println(a-b);
+        if ((a > b) && (a-b)>=num){return true;};
+        if ((a < b) && (b-a)>=num){return true;};
+        return false;
+    }
 }
-
