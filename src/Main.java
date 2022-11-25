a44789156d8a624adaa020de695816fb9bfccc89
Naumenco <naumenco@gmail.com>
2022-11-25 15:19:50 +0300

    Condition 3

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,25 @@
 import java.util.Scanner; // импорт сканера
-/* Безопасное деление. Дана следующая сигнатура метода:
+/* Максимум. Дана следующая сигнатура метода:
 
-public static int safeDiv(int x, int y);
-Необходимо реализовать метод таким образом, чтобы он возвращал деление x на y,
-и при этом гарантировал, что не будет выкинута ошибка деления на 0.
-При делении на 0 следует вернуть из метода число 0. Подсказка: смотри ограничения на операции типов данных.
+public static int max(int x, int y);
+Необходимо реализовать метод таким образом, чтобы он возвращал максимальное значение из двух полученных
+ методом чисел.
 
 Пример 1:
-x=5  y=0
-результат: 0
+x=5  y=7
+результат: 7
 Пример 2:
-x=8  y=2
-результат: 4
+x=8  y=-1
+результат: 8
  */
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Введите целое число (знаков в числе 1): ");
-        // Scanner reader = new Scanner(System.in);
-        // char charNumber = reader.next().charAt(0);
 
-        System.out.println("результат:" + safeDiv(-27,0));
+        System.out.println("результат:" + max(27,200));
     }
-    public static int safeDiv(int x, int y)
+    public static int max(int x, int y)
     {
-        if (y==0) {return 0;};
-        return x/y;
+        if (x>=y) {return x;};
+        return y;
     }
 }
