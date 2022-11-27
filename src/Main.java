3511cd056b2a33c89c9ba774ec08c911c4410038
Naumenco <naumenco@gmail.com>
2022-11-27 16:24:40 +0300

    Condition 9 release

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,29 @@
 import java.util.Scanner; // импорт сканера
-/* Тридцать пять. Дана следующая сигнатура метода:
+/* Волшебная шестерка. Дана следующая сигнатура метода:
 
-public static boolean is35(int x);
-Необходимо реализовать метод таким образом, чтобы он возвращал true, если число x делится нацело на 3 или 5.
-При этом, если оно делится и на 3, и на 5, то вернуть надо false.
-Подсказка: оператор % позволяет получить остаток от деления.
+public static boolean magic6(int x, int y);
+Необходимо реализовать метод таким образом, чтобы он возвращал true,
+если одно из принятых методом чисел равно шести, или их сумма равна шести,
+или разница между ними равна шести.
 
 Пример 1:
-x=5
-результат: true
+x=5  y=7
+результат: false
 Пример 2:
-x=8
-результат: false
+x=8  y=2
+результат: true
 Пример 3:
-x=15
-результат: false
+x=1  y=6
+результат: true
  */
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("результат: " + is35(15));
+        System.out.println("результат: " + magic6(8,-2));
     }
-    public static boolean is35(int x)
+    public static boolean magic6(int x, int y)
     {
-        if ((x%3==0) && (x%5==0)) {return false;}
-        if ((x%3==0) || (x%5==0)) {return true;}
+        if ((x==6) || (y==6) || (x+y==6) || (x-y==6) || (y-x==6)) {return true;}
         return false;
     }
 }
