996193bfbe1e3c70d18b698c51a5e51c8de4eaf1
Naumenco <naumenco@gmail.com>
2022-11-27 14:37:39 +0300

    Rebase (fix2)

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,11 @@ a=2 b=15 num=33
  */
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Введите целое число (знаков в числе 1): ");
-        // Scanner reader = new Scanner(System.in);
-        // char charNumber = reader.next().charAt(0);
 
-        System.out.println("результат:" + isInRange(2, 15, 13));
+        System.out.println("результат:" + isInRange(15, -2, 10));
     }
     public static boolean isInRange(int a, int b, int num)
     {
-        //System.out.println(a-b);
-        //if ((a > b) && (a-b)>=num){return true;};
-        //if ((a < b) && (b-a)>=num){return true;};
-        return (((a > b) && (a-b)>=num)||((a < b) && (b-a)>=num));
+         return (((a > b) && (num<=a) && (num>=b)) || ((a < b) && (num>=a) && (num<=b)));
     }
 }
