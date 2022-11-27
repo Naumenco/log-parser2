9f53aabe0a8827b9ac702dcebf19ae654dde88db
Naumenco <naumenco@gmail.com>
2022-11-27 15:58:34 +0300

    Condition 6 release

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,24 @@
 import java.util.Scanner; // импорт сканера
-/* Тройной максимум. Дана следующая сигнатура метода:
+/* Тройная сумма. Дана следующая сигнатура метода:
 
-public static int max3(int x, int y, int z);
-Необходимо реализовать метод таким образом, чтобы он возвращал максимальное из трех полученных методом чисел.
- Подсказка: идеальное решение включает всего две инструкции if и не содержит вложенных if.
+public static boolean sum3(int x, int y, int z);
+Необходимо реализовать метод таким образом, чтобы он возвращал true, если два любых числа (из трех принятых) можно сложить так, чтобы получить третье.
 
 Пример 1:
-x=5  y=7  z=7
-результат: 7
+x=5  y=7  z=2
+результат: true
 Пример 2:
 x=8  y=-1   z=4
-результат: 8
+результат: false
  */
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("результат: " + max3(-3, -7,-15));
+        System.out.println("результат: " + sum3(5, 7,2));
     }
-    public static int max3(int x, int y, int z)
+    public static boolean sum3(int x, int y, int z)
     {
-        if ((x>y)&&(x>z)) {return x;}
-        if ((y>x)&&(y>z))  {return y;}
-        return z;
+        if ((x+y==z) || (x+z==y) || (y+z==z)) {return true;}
+        return false;
     }
 }
