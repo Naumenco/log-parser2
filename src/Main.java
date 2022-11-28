2d9961312c48ad6c86abd70de0550ca1cd47acc4
Naumenco <naumenco@gmail.com>
2022-11-28 15:41:46 +0300

    Cycle 5

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,25 @@
 import java.util.Scanner; // импорт сканера
-/* Степень числа. Дана следующая сигнатура метода:
+/* Длина числа. Дана следующая сигнатура метода:
 
-public static int pow(int x, int y);
-Необходимо реализовать метод таким образом, чтобы он возвращал результат возведения x в степень y.
+public static int numLen(long x);
+Необходимо реализовать метод таким образом, чтобы он возвращал количество знаков в числе x.
 
-Подсказка: для получения степени необходимо умножить единицу на число x, и сделать это y раз, т.е.
- два в третьей степени это 1*2*2*2
-
+Подсказка:
+int у=123/10; // у будет иметь значение 12
 Пример:
-x=2
-y=5
-результат: 32
+x=12567
+результат: 5
  */
 public class Main {
     public static void main(String[]  args) {
-        System.out.println("результат: " + pow(2 ,5));
+        System.out.println("результат: " + numLen(2232343));
     }
-    public static int pow(int x, int y)
-    { int s=1;
-        for (int i=0; i<y;i++)
+    public static int numLen(long x)
+    { int c=0;
+        for (; x>0;x/=10)
         {
-            s = s*x;
+           c++;
         }
-        return s;
+        return c;
     }
 }
