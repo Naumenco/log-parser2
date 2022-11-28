876316e4fa14a8bcf43b8f1546e0e809f1d0b672
Naumenco <naumenco@gmail.com>
2022-11-28 15:18:44 +0300

    Cycle 3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner; // импорт сканера
-/* Числа наоборот. Дана следующая сигнатура метода:
+/* Четные числа. Дана следующая сигнатура метода:
 
-public static String reverseListNums(int x);
-Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой будут записаны все числа от x до 0 (включительно).
+public static String chet(int x);
+Необходимо реализовать метод таким образом, чтобы он возвращал строку, в которой будут записаны все
+ четные числа от 0 до x (включительно). Подсказа для обеспечения качества кода:
+ инструкцию if использовать не следует.
 
 Пример:
-x=5
-результат: “5 4 3 2 1 0”
+x=9
+результат: “0 2 4 6 8”
  */
 public class Main {
     public static void main(String[]  args) {
-        System.out.println("результат: " + listNums(5));
-
+        System.out.println("результат: " + chet(50));
     }
-    public static String listNums(int x)
+    public static String chet(int x)
     { String s="";
-        for (int i=x; i>=0;i--)
+        for (int i=0; i<=x;i+=2)
         {
-            s += String.valueOf(i)+" ";
+            s += i+" ";
         }
         return s;
     }
