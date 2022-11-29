69bd9adc277770516c2c500ab131cbfebc352795
Naumenco <naumenco@gmail.com>
2022-11-29 21:23:32 +0300

    Cycle 9

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,41 @@
 import java.util.Scanner; // импорт сканера
-/* Левый треугольник. Дана следующая сигнатура метода:
+/* Правый треугольник. Дана следующая сигнатура метода:
 
-public static void leftTriangle(int x);
-Необходимо реализовать метод таким образом, чтобы он выводил на экран треугольник из символов ‘*’
-у которого х символов в высоту, а количество символов в ряду совпадает с номером строки.
+public static void rightTriangle(int x);
+Необходимо реализовать метод таким образом, чтобы он выводил на экран треугольник из символов ‘*’ у
+которого х символов в высоту, а количество символов в ряду совпадает с номером строки,
+при этом треугольник выровнен по правому краю.
+1Подсказка: перед символами ‘*’ следует выводить необходимое количество пробелов.
 
 Пример 1:
-x=2
+x=3
 результат:
-*
-**
+    *
+  **
+***
 Пример 2:
 x=4
 результат:
-*
-**
-***
+      *
+    **
+  ***
 ****
  */
 public class Main {
-    public static void main(String[]  args) {
-        leftTriangle(7);
+    public static void main(String[] args) {
+        rightTriangle(4);
     }
-    public static void leftTriangle(int x)
+    public static void rightTriangle(int x)
     {
-        int count=0;
-            for (int i=0; i<x; i++){
-                for (int j=0; j<x; j++){
-                    System.out.print(" * ");
-                }
-                System.out.println("");
+       for (int i = 1; i <= x; i++) {
+            for (int sp = 1; sp <= x - i; sp++) {
+                System.out.print("  ");
             }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
         }
-
+    }
 }
 
