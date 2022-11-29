4009ae121cfaa0f699bccfa35e7a07cf1557f64c
Naumenco <naumenco@gmail.com>
2022-11-29 19:46:18 +0300

    Cycle 8

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,35 @@
 import java.util.Scanner; // импорт сканера
-/* Квадрат. Дана следующая сигнатура метода:
+/* Левый треугольник. Дана следующая сигнатура метода:
 
-public static void square(int x);
-Необходимо реализовать метод таким образом, чтобы он выводил на экран квадрат из символов ‘*’ размером х, у которого
-х символов в ряд и х символов в высоту. Подсказка: метод System.out.print выводит без перехода на следующую строку,
-в то время как System.out.println выводит с переходом.
+public static void leftTriangle(int x);
+Необходимо реализовать метод таким образом, чтобы он выводил на экран треугольник из символов ‘*’
+у которого х символов в высоту, а количество символов в ряду совпадает с номером строки.
 
 Пример 1:
 x=2
 результат:
-**
+*
 **
 Пример 2:
 x=4
 результат:
-****
-****
-****
+*
+**
+***
 ****
  */
 public class Main {
     public static void main(String[]  args) {
-        square(7);
+        leftTriangle(7);
     }
-    public static void square(int x)
+    public static void leftTriangle(int x)
     {
         int count=0;
-            for (int i=0; i<x*x; i++){
-                if (count==x)
-                {
-                    System.out.println (""); count =0;
+            for (int i=0; i<x; i++){
+                for (int j=0; j<x; j++){
+                    System.out.print(" * ");
                 }
-            count++;
-            System.out.print (" * ");
+                System.out.println("");
             }
         }
 
